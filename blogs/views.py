b7039709f0acc1e6b159b171c2ac0e8920f8b191
munjal patel <munjalpatel@munjal-patels-MacBook-Pro.local>
2016-09-04 11:50:31 +0530

    Cretead topics url, views and templates

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Topic
 # Create your views here.
 def index(request):
 	"""The home page for Blog"""
 	return render(request, 'blogs/index.html')
+
+def topics(request):
+	"""Show all topics"""
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'blogs/topics.html', context)
